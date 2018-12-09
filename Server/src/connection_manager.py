f2cff9773b793da54d2e37f43580ce423dee17e1
Sumit Paroothi <paroothisumit@gmail.com>
2018-12-10 01:57:19 +0530

    Used property file for mysql config

--- Server/src/connection_manager.py
+++ Server/src/connection_manager.py
@@ -1,3 +1,5 @@
+import configparser
+
 from mysql import connector
 
 
@@ -6,19 +8,20 @@ class DB:
     _db_cur = None
 
     def __init__(self):
-
-        self._db_connection = connector.connect(user='root',db='serverdb',password='sumit2000',auth_plugin='mysql_native_password')
+        db_config = configparser.ConfigParser()
+        db_config.read('../mysql-properties.ini')
+        print(type(dict(db_config['mysql_config'])))
+        self._db_connection = connector.connect(**dict(db_config['mysql_config']))
         self._db_cur = self._db_connection.cursor(buffered=True)
 
     def query(self, query, params):
-
         self._db_cur.execute(query, params)
-        print('Executed query:  '+query+'   params:'+str(params))
+        print('Executed query:  ' + query + '   params:' + str(params))
         self._db_connection.commit()
         return self._db_cur
+
     def __del__(self):
         self._db_connection.close()
-
 
 
 def __del__(self):
